0525f91c082d032ab186e5d224e3178f470f1209
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2023-04-09 22:41:42 -0500

    Changed privacy of methods

--- autosortfolder.py
+++ autosortfolder.py
@@ -5,6 +5,7 @@ from helpers import check_for_directory
 from os import listdir
 from sys import exit
 from sortingalgorithms import SORTING_METHODS
+import logging
 
 class AutoSortFolder:
     def __init__(self, root:str, update_interval:int=1):
@@ -12,19 +13,19 @@ class AutoSortFolder:
         self._timer     = RepeatedTimer(update_interval, self._root_update)
         self._sorter    = FileSorter(self.root)
 
-    def print_methods(self):
+    def _print_methods(self):
         print("--SORTING METHODS--")
         for i, method in enumerate(SORTING_METHODS):
             print(f"{i+1}) {method.name}")
         print(f"{i+2}) Back")
 
-    def main_menu(self):
+    def _main_menu(self):
         while True:
             print("--MAIN MENU--\n1) Select Sorting Method\n2) Start\n3) Quit")
             userInput = input(">> ")
             match (userInput):
                 case "1":
-                    self.menu_sorting_methods()
+                    self._menu_sorting_methods()
                 case "2":
                     if self._sorter.method != None:
                         break
@@ -35,8 +36,8 @@ class AutoSortFolder:
                 case _:
                     print("ERROR: Please choose one of the options above.")
     
-    def menu_sorting_methods(self):
-        self.print_methods()
+    def _menu_sorting_methods(self):
+        self._print_methods()
         while True:
             userInput = input(">> ")
             match (userInput):
@@ -50,7 +51,7 @@ class AutoSortFolder:
                     self._sorter.method = SORTING_METHODS[2]
                     break
                 case "4":
-                    self.menu_custom_sorting()
+                    self._menu_custom_sorting()
                     self._sorter.method = SORTING_METHODS[3]
                     break
                 case "5":
@@ -62,20 +63,21 @@ class AutoSortFolder:
         if self._sorter.method != None:
             check_for_directory(f"{self.root}/{self._sorter.method.name.lower()}")
     
-    def menu_custom_sorting(self):
+    def _menu_custom_sorting(self):
         pass
 
     def _root_update(self):
+        '''Updates the root directory'''
         for file in listdir(self.root):
             if not self._sorter.sort_file(self.root, f"{self.root}/{file}"):
-                print("Attempted to sort off-limits file.")
+                logging.debug(msg="Attempted to sort off-limits file.")
             else:
-                print(f"Sorted file '{file}'.")
+                logging.debug(msg=f"Sorted file '{file}'.")
 
-        print("Updated root!")
+        logging.debug("Updated root!")
 
     def run(self):
-        self.main_menu()    # Start the menu of the program
+        self._main_menu()    # Start the menu of the program
         print(f"Sorting by: {self._sorter.method.name}")
 
         self._timer.start() # Start the update timer
